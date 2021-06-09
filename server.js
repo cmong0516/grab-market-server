e25b48a0ebb0f6475bfb994d7dccc574a43906bc
Mong <Mong@Mong-Pro-16.local>
2021-06-09 17:32:28 +0900

    banner추가

--- server.js
+++ server.js
@@ -17,6 +17,21 @@ const upload = multer({
 
 app.use(express.json());
 app.use(cors());
+app.use('/uploads', express.static('uploads'));
+
+app.get('/banners', (req,res)=>{
+    models.Banner.findAll({
+        limit : 2
+    }).then((result)=>{
+        res.send({
+            banners: result,
+        });
+    }).catch((error)=>{
+        console.error(error);
+        res.status(500).send('에러가 발생했습니다.');
+    })
+})
+
 app.get("/products", (req, res) => {
     models.Product.findAll({
         order: [["createdAt", "DESC"]],
@@ -30,20 +45,21 @@ app.get("/products", (req, res) => {
         })
         .catch((error) => {
             console.error(error);
-            res.send("에러 발생");
+            res.status(400).send("에러 발생");
         });
 });
 app.post("/products", (req, res) => {
     const body = req.body;
-    const { name, description, price, seller } = body;
-    if (!name || !description || !price || !seller) {
-        res.send("모든 필드를 입력해주세요");
+    const { name, description, price, seller, imageUrl } = body;
+    if (!name || !description || !price || !seller || !imageUrl) {
+        res.status(400).send("모든 필드를 입력해주세요");
     }
     models.Product.create({
         name,
         description,
         price,
         seller,
+        imageUrl,
     })
         .then((result) => {
             console.log("상품 생성 결과 : ", result);
@@ -53,7 +69,7 @@ app.post("/products", (req, res) => {
         })
         .catch((error) => {
             console.error(error);
-            res.send("상품 업로드에 문제가 발생했습니다");
+            res.status(400).send("상품 업로드에 문제가 발생했습니다");
         });
 });
 
@@ -73,7 +89,7 @@ app.get("/products/:id", (req, res) => {
         })
         .catch((error) => {
             console.error(error);
-            res.send("상품 조회에 에러가 발생했습니다");
+            res.status(400).send("상품 조회에 에러가 발생했습니다");
         });
 });
 
