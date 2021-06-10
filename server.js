b438c2e290f643b3118d7232733d2bc52db57af7
Mong <Mong@Mong-Pro-16.local>
2021-06-10 17:48:42 +0900

    구매버튼 추가

--- server.js
+++ server.js
@@ -2,40 +2,48 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const models = require("./models");
-const port = 8080;
-const multer = require('multer');
+const multer = require("multer");
 const upload = multer({
     storage: multer.diskStorage({
         destination: function (req, file, cb) {
-            cb(null, 'uploads/')
+            cb(null, "uploads/");
         },
         filename: function (req, file, cb) {
             cb(null, file.originalname);
-        }
-    })
+        },
+    }),
 });
-
+const port = 8080;
 app.use(express.json());
 app.use(cors());
-app.use('/uploads', express.static('uploads'));
-
-app.get('/banners', (req,res)=>{
+app.use("/uploads", express.static("uploads"));
+app.get("/banners", (req, res) => {
     models.Banner.findAll({
-        limit : 2
-    }).then((result)=>{
-        res.send({
-            banners: result,
-        });
-    }).catch((error)=>{
-        console.error(error);
-        res.status(500).send('에러가 발생했습니다.');
+        limit: 2,
     })
-})
-
+        .then((result) => {
+            res.send({
+                banners: result,
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send("에러가 발생했습니다");
+        });
+});
 app.get("/products", (req, res) => {
     models.Product.findAll({
         order: [["createdAt", "DESC"]],
         attributes: ["id", "name", "price", "createdAt", "seller", "imageUrl"],
+        attributes: [
+            "id",
+            "name",
+            "price",
+            "createdAt",
+            "seller",
+            "imageUrl",
+            "soldout",
+        ],
     })
         .then((result) => {
             console.log("PRODUCTS : ", result);
@@ -54,13 +62,7 @@ app.post("/products", (req, res) => {
     if (!name || !description || !price || !seller || !imageUrl) {
         res.status(400).send("모든 필드를 입력해주세요");
     }
-    models.Product.create({
-        name,
-        description,
-        price,
-        seller,
-        imageUrl,
-    })
+    models.Product.create({ description, price, seller, imageUrl, name })
         .then((result) => {
             console.log("상품 생성 결과 : ", result);
             res.send({
@@ -72,7 +74,6 @@ app.post("/products", (req, res) => {
             res.status(400).send("상품 업로드에 문제가 발생했습니다");
         });
 });
-
 app.get("/products/:id", (req, res) => {
     const params = req.params;
     const { id } = params;
@@ -92,13 +93,35 @@ app.get("/products/:id", (req, res) => {
             res.status(400).send("상품 조회에 에러가 발생했습니다");
         });
 });
-
 app.post("/image", upload.single("image"), (req, res) => {
     const file = req.file;
     console.log(file);
     res.send({
         imageUrl: file.path,
     });
+});
+
+app.post("/purchase/:id", (req, res) => {
+    const { id } = req.params;
+    models.Product.update(
+        {
+            soldout: 1,
+        },
+        {
+            where: {
+                id,
+            },
+        }
+    )
+        .then((result) => {
+            res.send({
+                result: true,
+            });
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send("에러가 발생했습니다.");
+        });
 });
 
 app.listen(port, () => {
